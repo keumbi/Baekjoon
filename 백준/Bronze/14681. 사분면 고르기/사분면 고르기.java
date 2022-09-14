fbd68f1f88595b489628d0c9e2e0b5019236cfe9
Keumbi Park <rmaqldi@gmail.com>
2022-09-15 00:14:25 +0900

    [Bronze V] Title: 사분면 고르기, Time: 208 ms, Memory: 17688 KB -BaekjoonHub

--- 백준/Bronze/14681. 사분면 고르기/사분면 고르기.java
+++ 백준/Bronze/14681. 사분면 고르기/사분면 고르기.java
@@ -3,16 +3,13 @@ import java.util.Scanner;
 public class Main{
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int x = scanner.nextInt();
-        int y = scanner.nextInt();
-        if(x > 0){
-            if(y > 0) System.out.println(1);
-            else System.out.println(4);
-        }
-        else {
-            if (y > 0) System.out.println(2);
-            else System.out.println(3);
-        }
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        // xy ++ = 1, -+ = 2, -- =  3, +- =4
+        if(a > 0 && b > 0) System.out.print(1);
+        else if(a > 0 && b < 0) System.out.print(4);
+        else if(a < 0 && b < 0) System.out.print(3);
+        else System.out.print(2);
 
     }
 }

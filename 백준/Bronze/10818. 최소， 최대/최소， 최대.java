25e324be1b18d9ec63a6439b5f10a85df7ce581d
Keumbi Park <rmaqldi@gmail.com>
2022-09-20 01:59:39 +0900

    [Bronze III] Title: 최소, 최대, Time: 2540 ms, Memory: 324884 KB -BaekjoonHub

---
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.java
@@ -0,0 +1,19 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args)  {
+        Scanner in = new Scanner(System.in);
+
+        int n = in.nextInt();
+        int [] arr = new int[n];
+
+        for (int i = 0; i < n; i ++){
+            arr[i]= in.nextInt();
+        }
+        in.close();
+        Arrays.sort(arr);
+        System.out.print(arr[0] + " " + arr[n - 1]);
+
+    }
+}
